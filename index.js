22f8041abea6c31ab896754f244b910500a211ad
github username <github email address>
2020-01-25 20:39:23 -0700

    finished for assessment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+function writeCards(array, event){
+    const newArray = []
+
+    for(let i=0; i < array.length; i++){
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray
+}
